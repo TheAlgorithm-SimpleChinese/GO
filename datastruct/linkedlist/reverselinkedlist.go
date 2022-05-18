edefebf0db9b3c21a9abc1c71870b29a277e3eec
LiuYun <iwtmbtly@gmail.com>
2022-05-19 01:06:58 +0800

    数据结构：链表

---
+++ datastruct/linkedlist/reverselinkedlist.go
@@ -0,0 +1,12 @@
+// -*- coding: utf-8 -*-
+//author: liuyang
+//software: GoLand
+//file: reverselinkedlist.go
+//time: 2022/5/19 1:04
+
+package linkedlist
+
+type Node struct {
+	Data int
+	P    *Node
+}
